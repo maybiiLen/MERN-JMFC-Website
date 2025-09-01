e50ba60723252686028f99244dd4423983dec930
Henry Pham <henrypham6719@gmail.com>
2025-09-01 00:36:03 -0500

    Added Admin Login
    made a admin login so they can change stat's of player

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,24 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./db.js";
 
+
 dotenv.config();
 
 const app = express();
 
 connectDB();
+//middleware    
+function adminAuth(req,res,next){
+    const adminPass = process.env.ADMIN_SECRET;
+    if(!adminPass) return res.status(500).json({ message : "this the wrong number lil bro"});
+
+    const provided = req.header('x-admin-password') || req.body?.adminSecret || req.query?.adminSecret;
+    if(provided !== adminPass) 
+        return res.status(403).json({ message : "Access denied"
+    });
+    next();
+}
+
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +28,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req,res) => {
     res.send("API is running...");
+});
+app.get('/admin', adminAuth, (req,res) => {
+    res.json({ok : true, msg: 'admin access granted'})
 });
 
 const PORT = process.env.PORT || 5000;
